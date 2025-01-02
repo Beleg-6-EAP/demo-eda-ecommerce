eedc59bbedc644898066d182dcabddff0b00b6b7
bruderj15 <julian.bruder@outlook.com>
2025-01-02 17:01:48 +0100

    init

---
+++ src/main/java/demo/eda/DemoEdaEcommerceApplication.java
@@ -0,0 +1,13 @@
+package demo.eda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEdaEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEdaEcommerceApplication.class, args);
+    }
+
+}
