f61251c84189cef28d49560a2ec03054254ab9bd
bruderj15 <julian.bruder@outlook.com>
2025-01-21 17:53:15 +0100

    test: add `AllArgsConstructor` to `Order`

--- src/main/java/demo/eda/model/Order.java
+++ src/main/java/demo/eda/model/Order.java
@@ -1,10 +1,12 @@
 package demo.eda.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
+@AllArgsConstructor
 @Document(collection = "orders")
 public class Order {
 
